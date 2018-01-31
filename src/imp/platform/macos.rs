3969f2cf352b97023bdcad2847bd2bdc81e9efd7
Jimmy Brisson <jimbri01@u201251-mac.usa.arm.com>
2018-01-30 18:00:11 -0600

    Fix mac os builds with recent nightlies

--- src/imp/platform/macos.rs
+++ src/imp/platform/macos.rs
@@ -7,7 +7,7 @@ pub const USE_AUTHOR: bool = false;
 
 pub fn get_app_dir(t: AppDataType) -> Result<PathBuf, AppDirsError> {
     let dir_base: Result<PathBuf, AppDirsError> = if t.is_shared() {
-        Ok(Component::RootDir.as_ref().into())
+        Ok(Component::RootDir.as_os_str().into())
     } else {
         home_dir().ok_or_else(|| AppDirsError::NotSupported)
     };
