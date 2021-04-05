e94d67252d7880b922c86cb430e35ed01d32ce93
Andy Barron <andrewlbarron@gmail.com>
2021-04-04 23:08:11 -0700

    Fix .gitignore

--- target/doc/search-index.js
+++
@@ -1,4 +0,0 @@
-var searchIndex = JSON.parse('{\
-"app_dirs":{"doc":"Deprecated crateUse <code>directories</code> instead :)","i":[],"p":[]}\
-}');
-addSearchOptions(searchIndex);initSearch(searchIndex);
--- target/doc/source-files.js
+++
@@ -1,3 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["app_dirs"] = {"name":"","files":["lib.rs"]};
-createSourceSidebar();
